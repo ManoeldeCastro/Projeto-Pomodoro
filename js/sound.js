5a62d007b0340f2dd3530e510a466eb84b8325f6
Manoel Dias <115195961+ManoeldeCastro@users.noreply.github.com>
2022-11-13 12:12:59 -0300

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -4,11 +4,11 @@ export default function () {
   )
 
   const fundoAudio = new Audio(
-    "https://github.com/ManoeldeCastro/Explorer/blob/main/Stage%2005%20-%20Avan%C3%A7ando%20no%20JavaScript/testando/Tempo/audio/piano.mp3?raw=true"
+    "https://github.com/ManoeldeCastro/Explorer/blob/main/Stage%2005%20-%20Avan%C3%A7ando%20no%20JavaScript/testando/Tempo/audio/piano2.mp3?raw=true"
   )
 
   const timerFim = new Audio(
-    "https://github.com/ManoeldeCastro/Explorer/blob/main/Stage%2005%20-%20Avan%C3%A7ando%20no%20JavaScript/testando/Tempo/audio/fim2.mp3?raw=true"
+    "https://github.com/ManoeldeCastro/Explorer/blob/main/Stage%2005%20-%20Avan%C3%A7ando%20no%20JavaScript/testando/Tempo/audio/fim3.mp3?raw=true"
   )
 
   fundoAudio.loop = true
